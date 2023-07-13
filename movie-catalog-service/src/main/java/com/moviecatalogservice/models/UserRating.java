f57ba3f68bd8e4e232e8e77a27259fee63a4af75
katlaang <eunice.kamau@hotmail.com>
2023-07-14 00:33:20 +0300

    refactor + fix movieSummary

--- movie-catalog-service/src/main/java/com/moviecatalogservice/models/UserRating.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/models/UserRating.java
@@ -1,12 +1,14 @@
 package com.moviecatalogservice.models;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserRating {
 
     private String userId;
