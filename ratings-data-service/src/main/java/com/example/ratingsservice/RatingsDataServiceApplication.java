ac741e10d2fae69536e0ae172b278cd52fbd7b57
katlaang <eunice.kamau@hotmail.com>
2023-07-14 11:11:52 +0300

    refactoring + remove comments

--- ratings-data-service/src/main/java/com/example/ratingsservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/RatingsDataServiceApplication.java
@@ -8,8 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class RatingsDataServiceApplication {
 
-//     KieSession kieSession = new DroolsRuleConfig().initialize();
-
     public static void main(String[] args) {
         SpringApplication.run(RatingsDataServiceApplication.class, args);
     }
